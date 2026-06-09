7ba257bde55bd02e1c5f98cdcca062b4ffa0b719
Chulumanco <manco.last@gmail.com>
2026-06-09 21:49:20 +0200

    I have design the Admin UI login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Admin from "./frontend/pages/admin/admin";
+
 const App = () => {
-  return ( <h1>
-  Welcome to the Pizza Delivery App!
-  </h1> );
-}
- 
+  return (
+    <>
+      {/* <h1>Welcome to the Pizza Delivery App</h1> */}
+      <Admin />
+    </>
+  );
+};
+
 export default App;
