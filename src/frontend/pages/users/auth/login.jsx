fa706c63a0fb967d5259c7d3ebf4541b732e6b02
Chulumanco <manco.last@gmail.com>
2026-06-16 12:00:42 +0200

    I have added icons and now I am done with the UI I will be moving from login user to sign up user before that I will have to check what to take and make it commons for user

--- src/frontend/pages/users/auth/login.jsx
+++ src/frontend/pages/users/auth/login.jsx
@@ -1,8 +1,10 @@
+import { FcGoogle } from "react-icons/fc";
+
 const Login = () => {
   return (
     <div className="flex w-screen h-screen bg-[url(.\images\loginBG.png)] bg-no-repeat pl-8 justify-content-start items-center ">
-      <form className="flex flex-col gap-4 bg-white/8 p-8">
-        <h1 className="">Welcome Back!</h1>
+      <form className="flex flex-col gap-4 bg-white/8 p-8 text-surface">
+        <h1 className="text-white">Welcome Back!</h1>
         <div className="flex flex-col gap-4">
           <input
             type="text"
@@ -26,7 +28,10 @@ const Login = () => {
           Login
         </button>
               <h1 className="font-bold text-3xl flex items-center gap-4 before:h-0.5 before:bg-accent before:flex-1 after:h-0.5 after:bg-accent after:flex-1">OR</h1>
-              
+              <div className="flex justify-center gap-8 w-100">
+                <FcGoogle className="text-4xl"/>
+              </div>
+
       </form>
 
     </div>
