fb16d17a5fb7ac6f60107c7013b896b080a9c20c
Chulumanco <manco.last@gmail.com>
2026-06-16 12:59:54 +0200

    I am done with the ui for sign up for now, it is time to move into forgot password

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import Admin from "./frontend/pages/admin/admin";
-import Login from "./frontend/pages/users/auth/login";
+// import Login from "./frontend/pages/users/auth/login";
+import SignUp from "./frontend/pages/users/auth/signup";
 
 const App = () => {
   return (
     <>
       {/* <h1>Welcome to the Pizza Delivery App</h1> */}
       {/* <Admin /> */}
-      <Login />
+      {/* <Login /> */}
+      <SignUp />
     </>
   );
 };
--- src/frontend/pages/users/auth/signup.jsx
+++ src/frontend/pages/users/auth/signup.jsx
@@ -1,7 +1,47 @@
+import Button from "../../../common/button/button";
 import Form from "../../../common/form/form";
 
 const SignUp = () => {
-  return <Form></Form>;
+  return (
+    <Form title={"Sign Up!"}>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Email"
+        className="input"
+      />
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Full Name"
+        className="input"
+      />
+      <input
+        type="tel"
+        name="phone"
+        id="phone"
+        placeholder="Phone Number"
+        className="input"
+      />
+      <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            className="input"
+          />
+      <input
+            type="password"
+            name="password2"
+            id="password2"
+            placeholder="Confirm Password"
+            className="input"
+          />
+          <Button first={false} name={"Sign Up"} onClick={() => console.log("pressed")}/>
+    </Form>
+  );
 };
 
 export default SignUp;
