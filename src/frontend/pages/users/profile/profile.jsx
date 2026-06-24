654e00d6de6f066e3b1b65bb46a15f35bbceb8a3
Chulumanco <manco.last@gmail.com>
2026-06-24 23:11:01 +0200

    profile.jsx - I have just added a mock up of the update form

--- src/frontend/pages/users/profile/profile.jsx
+++ src/frontend/pages/users/profile/profile.jsx
@@ -1,7 +1,18 @@
 const Profile = () => {
     return ( 
-        <div className="flex w-screen h-screen">
+        <div className="flex-col w-screen h-screen">
             <h1>Profile</h1>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input type="text" placeholder="Chulumanco" id="name"/>
+                <label htmlFor="surname">Surame</label>
+                <input type="text" placeholder="Mdutyana" id="surname"/>
+                <label htmlFor="email">Email</label>
+                <input type="email" placeholder="test@gmail.com" id="email"/>
+                <label htmlFor="phone">Phone Number</label>
+                <input type="tel" placeholder="+27 79 834 8373" id="phone"/>
+                <button>Update</button>
+            </form>
         </div>
      );
 }
